e5dd95ba4ba9733522f8fc2df23d15e47990a72a
dsolerac <daniel.solera@code.seat>
2025-05-08 09:35:31 +0200

    initial commit

---
+++ src/main/kotlin/com/seat/consorcial_backend/ConsorcialBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.seat.consorcial_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsorcialBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsorcialBackendApplication>(*args)
+}
